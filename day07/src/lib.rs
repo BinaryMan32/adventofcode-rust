b789e8450314ca51d3846767736f866db518dd3e
Fred Fettinger <fettinge@gmail.com>
2021-12-07 00:49:27 -0600

    day07 part 2

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -14,6 +14,16 @@ pub fn part1(input: &Vec<&str>) -> i64 {
         .unwrap()
 }
 
+fn cost(a: i64, b: i64) -> i64 {
+    let n = (a - b).abs();
+    n * (n + 1) / 2
+}
+
 pub fn part2(input: &Vec<&str>) -> i64 {
-    0
+    let positions = parse_positions(input[0]);
+    let max_position = positions.iter().max().unwrap();
+    (0..(max_position+1))
+        .map(|p| positions.iter().map(|&x| cost(x, p)).sum())
+        .min()
+        .unwrap()
 }
