4399fb4427ea69b1ac7d4ff9958f59afa90bbace
Fred Fettinger <fettinge@gmail.com>
2021-12-07 00:45:43 -0600

    day07 solution part 1

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -1,5 +1,17 @@
+fn parse_positions(input: &str) -> Vec<i64> {
+    input
+        .split(",")
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect()
+}
+
 pub fn part1(input: &Vec<&str>) -> i64 {
-    0
+    let positions = parse_positions(input[0]);
+    let max_position = positions.iter().max().unwrap();
+    (0..(max_position+1))
+        .map(|p| positions.iter().map(|x| (x-p).abs()).sum())
+        .min()
+        .unwrap()
 }
 
 pub fn part2(input: &Vec<&str>) -> i64 {
