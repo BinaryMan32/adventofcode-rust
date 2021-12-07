a336f315f4a165d9e1a46b493958ffcfb70a6d1f
Fred Fettinger <fettinge@gmail.com>
2021-12-07 00:57:30 -0600

    day07 solution: refactor to remove duplication

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -5,25 +5,27 @@ fn parse_positions(input: &str) -> Vec<i64> {
         .collect()
 }
 
-pub fn part1(input: &Vec<&str>) -> i64 {
-    let positions = parse_positions(input[0]);
+fn min_cost(positions: &Vec<i64>, cost_fn: fn (i64, i64) -> i64) -> i64 {
     let max_position = positions.iter().max().unwrap();
     (0..(max_position+1))
-        .map(|p| positions.iter().map(|x| (x-p).abs()).sum())
+        .map(|p| positions.iter().map(|&x| cost_fn(x, p)).sum())
         .min()
         .unwrap()
 }
 
-fn cost(a: i64, b: i64) -> i64 {
+fn cost1(a: i64, b: i64) -> i64 {
+    (a - b).abs()
+}
+
+pub fn part1(input: &Vec<&str>) -> i64 {
+    min_cost(&parse_positions(input[0]), cost1)
+}
+
+fn cost2(a: i64, b: i64) -> i64 {
     let n = (a - b).abs();
     n * (n + 1) / 2
 }
 
 pub fn part2(input: &Vec<&str>) -> i64 {
-    let positions = parse_positions(input[0]);
-    let max_position = positions.iter().max().unwrap();
-    (0..(max_position+1))
-        .map(|p| positions.iter().map(|&x| cost(x, p)).sum())
-        .min()
-        .unwrap()
+    min_cost(&parse_positions(input[0]), cost2)
 }
