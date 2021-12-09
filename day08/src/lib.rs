6d35a37a45a5d27b1547b7cdd5237c7153790f87
Fred Fettinger <fettinge@gmail.com>
2021-12-08 19:34:52 -0600

    day08: create DigitSet to wrap collections of char HashSets

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -2,26 +2,16 @@ use std::collections::HashSet;
 
 use lazy_static::lazy_static;
 
-struct Entry {
-    patterns: Vec<HashSet<char>>,
-    digits: Vec<HashSet<char>>,
+struct DigitSet {
+    digits: Vec<HashSet<char>>
 }
 
-lazy_static! {
-    // unique number of segments for digits 1, 4, 7, 8
-    static ref UNIQUE_SIZES: HashSet<usize> = HashSet::from_iter([2, 4, 3, 7]);
-}
-
-impl Entry {
-    fn parse(line: &str) -> Entry {
-        let parts = line.split(" | ").into_iter().collect::<Vec<_>>();
-        Entry {
-            patterns: parts[0].split(" ").into_iter()
+impl DigitSet {
+    fn parse(input: &str) -> DigitSet {
+        DigitSet {
+            digits: input.split(" ").into_iter()
                 .map(|x| x.chars().collect::<HashSet<char>>())
-                .collect(),
-            digits: parts[1].split(" ").into_iter()
-                .map(|x| x.chars().collect::<HashSet<char>>())
-                .collect(),
+                .collect()
         }
     }
 
@@ -33,11 +23,30 @@ impl Entry {
     }
 }
 
+struct Entry {
+    patterns: DigitSet,
+    digits: DigitSet,
+}
+
+lazy_static! {
+    // unique number of segments for digits 1, 4, 7, 8
+    static ref UNIQUE_SIZES: HashSet<usize> = HashSet::from_iter([2, 4, 3, 7]);
+}
+
+impl Entry {
+    fn parse(line: &str) -> Entry {
+        let parts = line.split(" | ").into_iter().collect::<Vec<_>>();
+        Entry {
+            patterns: DigitSet::parse(parts[0]),
+            digits: DigitSet::parse(parts[1]),
+        }
+    }
+}
+
 pub fn part1(input: &Vec<&str>) -> i64 {
     input.iter()
         .map(|line| Entry::parse(line))
-        .inspect(|entry| println!("{:?} ({})", entry.digits, entry.unique_digits()))
-        .map(|entry| entry.unique_digits() as i64)
+        .map(|entry| entry.digits.unique_digits() as i64)
         .sum()
 }
 
