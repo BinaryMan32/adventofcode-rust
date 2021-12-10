d26fb215cd0b58687432ac4ded18fe9e4e0ecb88
Fred Fettinger <fettinge@gmail.com>
2021-12-09 21:09:44 -0600

    day09 part2 solution

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -1,3 +1,5 @@
+use std::iter;
+
 struct HeightMap {
     cells: Vec<Vec<u8>>
 }
@@ -40,6 +42,32 @@ impl HeightMap {
             .map(|(r, c)| (self.cells[r][c] + 1) as i64)
             .sum()
     }
+
+    fn create_mask(&self) -> Vec<Vec<bool>> {
+        let num_cols = self.cells[0].len();
+        (0..self.cells.len()).map(|_r| iter::repeat(false).take(num_cols).collect()).collect()
+    }
+
+    fn find_basin_size(&self, r: usize, c: usize, mask: &mut Vec<Vec<bool>>) -> usize {
+        if mask[r][c] || self.cells[r][c] >= 9 {
+            0
+        } else {
+            mask[r][c] = true;
+            1
+                + (if r > 0 {self.find_basin_size(r - 1, c, mask)} else {0})
+                + (if r < self.cells.len() - 1 {self.find_basin_size(r + 1, c, mask)} else {0})
+                + (if c > 0 {self.find_basin_size(r, c - 1, mask)} else {0})
+                + (if c < self.cells[0].len() - 1 {self.find_basin_size(r, c + 1, mask)} else {0})
+        }
+    }
+
+    fn find_basin_sizes(&self) -> Vec<i64> {
+        let mut mask = self.create_mask();
+        self.find_low_points()
+            .into_iter()
+            .map(|(r, c)| self.find_basin_size(r, c, &mut mask) as i64)
+            .collect()
+    }
 }
 
 pub fn part1(input: &Vec<&str>) -> i64 {
@@ -48,5 +76,12 @@ pub fn part1(input: &Vec<&str>) -> i64 {
 }
 
 pub fn part2(input: &Vec<&str>) -> i64 {
-    0
+    let height_map = HeightMap::parse(input);
+    let mut basin_sizes = height_map.find_basin_sizes();
+    basin_sizes.sort();
+    println!("basin_sizes={:?}", basin_sizes);
+    basin_sizes.into_iter()
+        .rev()
+        .take(3)
+        .product()
 }
